14b30433bf014fb10a3e0eac5ea17ccadd142392
Mike Ng <iam@mikeng.io>
2023-03-28 17:07:16 +0800

    Initial commit

---
+++ test/aws-evm-tx-fifo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEvmTxFifo from '../lib/aws-evm-tx-fifo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-evm-tx-fifo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEvmTxFifo.AwsEvmTxFifoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
